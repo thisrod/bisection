322d3bae00b1a4fa9adc28508f58b74c4004135d
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2015-06-03 13:27:03 +1000

    going home

--- horbs.m
+++ horbs.m
@@ -1,38 +1,28 @@
-# Hartree orbitals in the trap plane
+% Hartree orbitals in the trap plane
 
-# Spectral matrix from Program 8 of 2000-Trefethen-Spectral
+% Spectral matrix from Program 8 of 2000-Trefethen-Spectral
 
-prinax
+prinax, [x, y, z, K] = loadk(0, U, r0);
 
-[x, y, z, K] = loadk(0, U, r0);
-[~,j0] = min(K, [], 2);  j0 = repmat(j0, [1,3,1]);
-d = size(j0);
-[i,j,k] = ndgrid(1:d(1), 1:d(2), 1:d(3));	% is there a special function for this?
+% interpolate through lowest 3 points along y to find trap bottom
+[~,j0] = min(K, [], 2);  j0 = repmat(j0, [1,3,1]);  d = size(j0);
+[i,j,k] = ndgrid(1:d(1), 1:d(2), 1:d(3));
 K3 = K(sub2ind(size(K), i(:), j(:)+j0(:), k(:)));  K3 = reshape(K3,d);
 K2 = K3(:,2,:) + (K3(:,1,:)-K3(:,3,:)).^2/8./(2*K3(:,2,:)-K3(:,1,:)-K3(:,3,:));
-figure; contour(squeeze(K2), (0:0.1:1).^2, '-k')
+K2 = squeeze(K2);
+figure, contour(squeeze(K2), (0:0.1:1).^2, '-k')
 
-if 1==0
-# construct a spectral transverse laplacian operator
-ns = size(K);  ns = ns([1 3]);  hs = 2*pi./ns;
-Ls = [range(x) range(z)].*ns/2./(ns-1);
-for i = 1:2
-	column = [-pi^2/(3*hs(i)^2)-1/6 ...
-		-0.5*(-1).^(1:ns(i)-1)./sin(hs(i)*(1:ns(i)-1)/2).^2];
-	D2 = (pi/Ls(i))^2*toeplitz(column);
-	switch i
-		case 1
-			dxx = D2;
-			Dxx = kron(eye(ns(2)), D2);
-		case 2
-			dzz = D2;
-			Dzz = kron(D2, eye(ns(1)));
-	endswitch
-endfor
+function D2=ssd(N,L)		% spectral second derivative
+	h = 2*pi./N;  L = L*N/(N-1);	% adjust to periodic grid
+	column = [-pi^2/(3*h^2)-1/6 ...
+		-0.5*(-1).^(1:N-1)./sin(h*(1:N-1)/2).^2];
+	D2 = (pi/L)^2*toeplitz(column);
+endfunction
+
+dxx = ssd(length(x), range(x)/2);  Dxx = kron(eye(length(z)), dxx);
+dzz = ssd(length(z), range(z)/2);  Dzz = kron(dzz, eye(length(x)));
 LAP = Dxx + Dzz;
-q = zeros(size(K(:)));
-HAM = 0.5*(-LAP+diag(K(:)+0.283*abs(q).^2));
+q = zeros(size(K2(:)));
+HAM = 0.5*(-LAP+diag(K2(:)+0.283*abs(q).^2));
 [Q, ks] = eig(HAM);
 [k,i] = min(ks);
-
-endif		% 1==0
