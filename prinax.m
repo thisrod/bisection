83e494c2bc3e1fbade7910f8209bebc4b96f2fa7
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2015-06-25 14:45:54 +1000

    polished qrtfit and prinax

--- prinax.m
+++ prinax.m
@@ -1,4 +1,5 @@
-# Find principal axes of the trap potential
+% initialise principal axes of the trap potential
+% run prinaxfigs to plot cross section
 
 N = 7e3;		# nominal number of atoms
 
@@ -40,16 +41,3 @@ U = U(:,k);  U([2 4 6 8]) = 0;
 for i = 1:3
 	U(:,i) = sign(U(i,i))*U(:,i)/norm(U(:,i));
 end
-
-# plot section through centre of mass
-[~,i0] = min(abs(z1));  [X,Y] = ndgrid(x,y);
-clf, hold on, axis([-4 4 100 106.5]), axis equal
-contour(x, y, K(:,:,i0).', (1:9).^2/0.288.^2), colormap([0 0 0])
-plot(X, Y, '+k', 'markersize', 2, 'color', [0.7 0.7 0.7])
-plot(r0(1), r0(2), 'ok', 'markersize', 4)
-text(0.05, 103.35, 'r_0', 'fontsize', 14)
-plot(1.35+103.15i+0.288*exp(2*pi*1i*(0:0.01:1)), '-r')
-text(1.4, 102.7, 'x_0', 'color', 'r', 'fontsize', 14)
-xlabel x,  ylabel y
-title(['Section of split trap, t = 17 ms, z = ' num2str(z(i0), 3)], 'fontsize', 14)
-text(1.5, 105.9, 'K^2=1/x_0^2, 4/x_0^2, 9/x_0^2, ...', 'fontsize', 14)
--- qrtfit.m
+++ qrtfit.m
@@ -1,40 +1,37 @@
 % fit quadratic/quartic potentials to the Vienna trap data, for interpolation around the bottom of the trap.
+% run qrtfitfigs to print the figures
 
 set(0, 'defaultaxesfontsize', 14, 'defaulttextfontsize', 14)
+prinax
 
-t = 13;  prinax,  [x, y, z, K] = loadk(t);
-[r0, wgt] = tcent(x,y,z,K);
-x1 = x - r0(1);  y1 = y - r0(2);  z1 = z - r0(3);
+cfs = repmat(nan, 5, 18);		% cols hold coefficients [x^4 1 x^2 y^2 z^2]
 
-% fit symmetric quartic to wells below second excited state
-[~,i0] = min(abs(y1));  [~,j0] = min(abs(z1));
-K1 = squeeze(K(:,i0,j0));
-[~,i1] = min(abs(x1));  ft = (K1 <= 50);
-xf = x(ft)';  kf = K1(ft);
-xcfs = [xf.^4 xf.^2 ones(size(xf))] \ kf;
-K2 = [x'.^4 x'.^2 ones(size(x'))]*xcfs;
-
-% r expands sample points over principal axes
-[X,Y,Z] = ndgrid(x1,y1,z1);  r = [X(:) Y(:) Z(:)]*U;
-rsdl = K(:) - [r(:,1).^4 r(:,1).^2 ones(numel(X),1)]*xcfs;
-cfs = ([r(:,2).^2 r(:,3).^2].*repmat(wgt,1,2)) \ (rsdl.*wgt);
-Kfit = [r(:,1).^4 r(:,1).^2 ones(numel(X),1) r(:,2).^2 r(:,3).^2]*[xcfs; cfs];
-Kfit = reshape(Kfit, size(K));
-
-figure, plot(x,K1,'-k', x, K2, '-r')
-title 'Fit along splitting axis'
-text(-1.5, 125, sprintf('%.2f x^4 %+.2f x^2 %+.2f', xcfs(1), xcfs(2), xcfs(3)), 'color', 'r')
-xlabel x, ylabel K^2
-axis([-3 3 0 200])
-
-figure
-for i = 1:2
-	r1 = r0+U*[0 0 z(j0)]';
-	subplot(1,2,i), hold on
-	contour(x, y, K(:,:,j0).', (1:10).^2/0.288.^2), colormap([0 0 0])
-	contour(x, y, Kfit(:,:,j0).', (1:10).^2/0.288.^2, '-r')
-	plot(r1(1), r1(2), 'ok', 'markersize', 4)
-	xlabel x, ylabel y, axis equal, axis([-4 4 100 106])
-	title(sprintf('Fit at t = %d, z = %.1f', t, z(j0)))
-	j0 = 70;
+tic
+for t = 0:17
+	[x, y, z, K] = loadk(t);
+	[r0, wgt] = tcent(x,y,z,K);
+	x1 = x - r0(1);  y1 = y - r0(2);  z1 = z - r0(3);
+	
+	% fit symmetric quartic to potential well below second excited state
+	% fit doesn't justify interpolating from nearest grid point to exact minimum
+	%	or rotating grid
+	[~,i1] = min(abs(x1));  [~,i2] = min(abs(y1));  [~,i3] = min(abs(z1));
+	K1 = squeeze(K(:,i2,i3));
+	xf = x(K1 <= 50)';
+	cfs(1:3, t+1) = [xf.^4 ones(size(xf)) xf.^2] \ K1(K1 <= 50);
+	
+	% r expands sample points over principal axes
+	% TODO filter out rows with tiny weights
+	[X,Y,Z] = ndgrid(x1,y1,z1);  r = [X(:) Y(:) Z(:)]*U;
+	rsdl = K(:) - [r(:,1).^4 ones(size(r(:,1))) r(:,1).^2]*cfs(1:3, t+1);
+	cfs(4:5, t+1) = ([r(:,2).^2 r(:,3).^2].*[wgt wgt]) \ (rsdl.*wgt);
+	
+	if t == 13
+		Ks = K;  Ks1 = K1;  rr = r0;  j0 = i3;
+		Kf = [r(:,1).^4 ones(size(r(:,1))) r.^2]*cfs(:,t+1);
+	endif
 endfor
+toc
+
+% set x^4 coefficient constant
+cfs(1,:) = mean(cfs(1,10:end));
