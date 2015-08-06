a6fd78769aa174d32e59389e4e26844f2079a375
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2015-08-06 11:43:24 +1000

    swapped axes to plot 1D graphs

--- BoseOne.m
+++ BoseOne.m
@@ -1,22 +1,27 @@
 function [e] = BoseOne()                        %%name of main function
 
-global cfs
-load cfs		% cols hold coefficients [x^4 1 x^2 y^2 z^2]
+% This script calls the trap axis x in order to plot 1D graphs along
+% it. The bisection axis is called y.  The graph labels match my thesis.
+
+gr1.xlabels = {'t', 'z', 'x'};
+
+global cfs,  load cfs
+cfs = cfs([1 2 3 5], :);  % column i has coefs of [y^4 1 y^2 x^2] at t/ms = i-1
 
 tmax = 5;
 
 icond.name =		'two dimensional Vienna trap, imaginary time initial state';
 icond.dimension =	3;
 icond.fields =		1; 
-icond.ranges =		[tmax,6,200];
+icond.ranges =		[tmax,200,6];
 icond.order =		0;
 icond.steps =		10*tmax;
 icond.step =		@nrmstp;
-icond.graphs =		[1 0 0 0];
+% icond.graphs =		[0 0 0 0];
 icond.initial =		@(r,w,in) ones(size(r.x));
 icond.da =		@Da;
 icond.linear =		@(D,in) 0.5*(D.x.^2 + D.y.^2); 
-icond.observe =	@(a,~,~) abs(a).^2;
+% icond.observe =	@(a,~,~) abs(a).^2;
 icond.ensembles =	[1 1 1];
 
 gr1.images =		[0];
@@ -31,13 +36,14 @@ monte.linear =		@(D,in) 0.5*1i*(D.x.^2 + D.y.^2);
 % monte.observe =	@(a,r,in) abs(a).^2 - 1/(2*in.dV);
 monte.observe =	@(a,r,in) phase(a,r,in,+1);
 monte.step =		@xMP;
-monte.ranges =	[17/1.368,6,200];
+monte.ranges =	[17/1.368,200,6];
 monte.noises =		[2 0];	% Re and Im
 monte.steps =		50;
 
 gr2 = gr1;
 gr2.images =		[5];
 gr2.olabels =		{'\phi_R'};
+gr2.pdimension =	2;
 
 e  =  xspde({icond, monte}, {gr1, gr2});
 
@@ -51,14 +57,16 @@ end
 
 function f = phase(a,r,in,dn)
 	m = in.dx;
-	m(3) = 0;
-	f = xint(a.*(dn*r.x > 0), m, in);
+	m(2) = 0;	% IWRT y
+	f = xint(a.*(dn*r.y > 0), m, in);
 	f = angle(f);
 end
 
+% FIXME D.R.Y.
+
 function da  =  Da(a,r,dt,dw)
 	global cfs
-	K = [r.x(:).^4 ones(size(r.x(:))) r.x(:).^2 r.y(:).^2]*cfs([1 2 3 5], 1);
+	K = [r.y(:).^4 ones(size(r.x(:))) r.y(:).^2 r.x(:).^2]*cfs(:, 1);
 	K = reshape(K, size(r.x));
 	K = min(K, 100);		% trim unphysical part from quartic fit
 	da = -0.5*(K + 0.2255*abs(a).^2).*a*dt;
@@ -67,8 +75,8 @@ end
 function da  =  Db(a,r,dt,dw)
 	global cfs
 	ts = (0:17)/1.368;
-	c = interp1(ts, cfs([1 2 3 5], :)', r.t)';
-	K = [r.x(:).^4 ones(size(r.x(:))) r.x(:).^2 r.y(:).^2]*c;
+	c = interp1(ts, cfs', r.t)';
+	K = [r.y(:).^4 ones(size(r.x(:))) r.y(:).^2 r.x(:).^2]*c;
 	K = reshape(K, size(r.x));
 	K = min(K, 100);		% trim unphysical part from quartic fit
 	da = -0.5*1i*(K + 0.2255*abs(a).^2).*a*dt;
