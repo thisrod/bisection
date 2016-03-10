c8611234524b71fdb03fd76c6eb5ad6b08e87002
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-03-10 17:11:52 +1100

    bdv has chemical potential and laplacian

--- Ground.m
+++ Ground.m
@@ -2,8 +2,9 @@ function Ground()                        %%name of main function
 
 vienna obervables
 order = vienna('initial');
+order = xinstrument(order, 'T', 'K', 'V');
 
-ground = bdv(order);
+ground = bdv(order, 0);
 ground = xinstrument(ground, 'R', 2);
 
 xspde({order, ground})
--- bdv.m
+++ bdv.m
@@ -1,4 +1,4 @@
-function out = bdv(in)
+function out = bdv(in,t)
 %BDV     XPDE input structure for Bogoliubov ground states
 % For now, set the field to the lagrangian of the input field.
 
@@ -8,17 +8,22 @@ for s = {'dimension', 'fields', 'ranges', 'c', 'points'}
 end
 
 out.points(1) = 2;  out.ranges(1) = 1;
-out.transfer = @tfr;
+out.transfer = @(w,r,a0,r0) tfr(t,w,r,a0,r0);
 
 end
 
-function a = tfr(w,r,a0,r0)
+function a = tfr(t,w,r,a0,r0)
 	% compute the Bogoliubov modes
 	% this only works for one sample
-	size(a0)
-	p = r.points,  g = r.ranges
+	r.t = t;
+	T = 1.44;  K = 16.85;  V = 19.7;
+	% kludge, read from graphs.
+	M = T + K + V;
+	p = r.points;  g = r.ranges;
 	Dxx = kron(eye(p(3)), ssd(p(2), g(2)));
 	Dyy = kron(ssd(p(3), g(3)), eye(p(2)));
-	a = (Dxx+Dyy)*a0.';
+	NL = r.c.K(r) + 2*r.c.rpsn*abs(a0).^2 - M;
+	NL = diag(NL(:));
+	a = NL*a0.';
 	a = a.';
 end
