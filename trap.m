16df746741e87172c725245f3824b66ee04a7713
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-03-18 12:08:07 +1100

    added guard fields to offer and make

--- trap.m
+++ trap.m
@@ -17,7 +17,7 @@ if data == [2 false true]
 end
 
 % potential defaults to free atoms
-in.a = offer(in.a, 'K', @(r) zeros(r.d.a));
+in.a = offer(in.a, 'K', @(r) zeros(r.d.a), 'g', 'LAP');
 
 LAPs = {@(D,r) D.x.^2, @(D,r) D.x.^2 + D.y.^2, @(D,r) D.x.^2 + D.y.^2 + D.z.^2};
 in.a.LAP = LAPs(in.dimension - 1);
@@ -27,12 +27,17 @@ in.linear = @(D,r) 0.5*1i*in.a.LAP(D,r);
 end
 
 
-function in = offer(in, field, value)
-	if ~isfield(in, field), in = setfield(in, field, value); end
+function in = offer(in, field, value, varargin)
+	% give in.field the default value, provided that has no value already and that in has the fields named in varargin 
+	if ~isfield(in, field) && ...
+		(isempty(varargin) || all(isfield(in, varargin)))
+		in = setfield(in, field, value);
+	end
 end
 
-function in = make(in, field, value)
+function in = make(in, field, value, varargin)
 	% FIXME comparing floats for equality
+	if ~(isempty(varargin) || all(isfield(in, varargin))), return, end
 	if isfield(in, field)
 		assert(getfield(in, field) == value);
 	else
