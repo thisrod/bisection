c58e4609da1f448e692b46731f861eb2cfe81f9f
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-03-18 14:07:21 +1100

    fix spelling of bdv

--- Pair.m
+++ Pair.m
@@ -2,13 +2,13 @@
 
 clear free
 free.name = 'free bosons in one dimension';
-free.ranges = [NaN 1e5];
 free.a.gamma = 1e3;
+free.a.N = 1e5;
 free = trap(free);
 
 ground = order(free, 1e5);
 ground.ranges(1) = 0.5;
 ground.points = [49 70];
 ground.steps = 30;
-ground = xinstrument(ground, 'n', 5, 'N', 1e5);
+ground = xinstrument(ground, 'n', 'N', 1e5);
 xspde(ground)
