4549e3dba93614faa7fd230c891235804eed9563
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-03-29 15:44:03 +1100

    debug script showing factor 100 disrepency in Laplacian eigenvalues

--- loadone.m
+++ loadone.m
@@ -15,8 +15,8 @@ figure, plot(r.x, LAP*a, r.x, K*a, r.x, r.a.g*diag(a.^2)*a)
 figure, semilogy(abs(ew), '.k'), title Eigenvalues
 % w = ck, k is around n/4L, healing length is (2*gamma)^{-1/2} in LL normalisation
 % CHECKME
-n = 1:length(ew);
-hold on, plot(n, (n-2)*sqrt(r.a.gamma)/(4*r.ranges(2)), '-k')
+n = 1:length(ew);  L = r.ranges(2);  L = L*gs/(gs-1);
+hold on, plot(n, (n-2)*sqrt(r.a.gamma)/(4*L), '-k')
 legend computed sound Location SouthEast
 
 figure, imagesc(bmod), colormap gray, title Eigenvectors
@@ -31,11 +31,24 @@ figure, semilogy(nms, '.k'), title 'Eigenvector norms'
 figure, plot(r.x, r.a.g*a.^2-mu, '-k', r.x, r.a.K(r), '-r')
 legend('\mu discrep.', 'K')
 
+figure
+k1 = 2*pi/L;
+plot(r.x, sin(k1*r.x'), '-k', r.x, -k1^-2*LAP*sin(k1*r.x'), '--r')
+hold on, k1 = 10*k1;
+plot(r.x, sin(k1*r.x'), '-k', r.x, -k1^-2*LAP*sin(k1*r.x'), '--r')
+plot(r.x, 
+title 'sine waves reproduced with spectral Laplacian'
+
 [sv,sw] = eig(-LAP, 'vector');
-figure, plot(sw, '.k'), title('laplacian eigenvalues')
+n = 1:length(sw);  k = k1*n/2;
+figure, plot(n, sw, '.k', n, k.^2, '-k'), title('laplacian eigenvalues')
 figure, imagesc(sv), colormap gray, title('laplacian eigenvectors')
 
 [osv,osw] = eig(-U1'*LAP*U1, 'vector');
 [~, i] = sort(abs(osw));  osw = osw(i);  osv = osv(:,i);
 figure, plot(real(osw), '.k'), title('orthogonal laplacian eigenvalues')
 figure, imagesc(real(U1*osv)), colormap gray, title('orthogonal laplacian eigenvectors')
+
+figure
+plot(r.x, sv(:,2:3), '-k', r.x, -100*k1^-2*LAP*sv(:,2:3), '--r')
+title 'numerical laplacian eigenvectors x100'
