041732adb257a1e9ebeac06ffd087271b7d71a6a
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-04-04 15:39:10 +1000

    fixed comparison 1D BdG spectrum

--- loadone.m
+++ loadone.m
@@ -4,22 +4,71 @@ load debug.mat
 
 gs = r.points(2);
 
-norm(imag(ew)) / norm(ew), norm(imag(ev), 'fro') / norm(ev, 'fro'), norm(imag(U), 'fro') / norm(U, 'fro')
+assert(isa(U, 'double'))
 
-[~, i] = sort(abs(ew));  ew = real(ew(i));  ev = ev(:,i);
-bmod = real(U*ev);  ev = real(ev);
+fprintf('DEBUGGING INFORMATION FOR 1D BdG MODES\n\n')
+fprintf('Machine epsilon: %.2e\n\n', eps)
+fprintf('Imaginary fraction of eigen-\n     values: %.2e\n     vectors: %.2e\n\n', ...
+	norm(imag(ew)) / norm(ew), ...
+	norm(imag(ev), 'fro') / norm(ev, 'fro'))
+
+assert(norm(imag(ew)) / norm(ew) < 1e-5), ew = real(ew);
+pew = ew(ew>0);  new = ew(ew<0);
+fprintf('Mismatch between +ive and -ive eigenvalues: %.2e\n\n', ...
+	norm(sort(pew) + sort(new, 'descend')))
+	
+fprintf('non-orthonormality of eigenvectors: %.2e\n', ...
+	norm(ev'*ev-eye(2*gs-2), 'fro'))
+
+% remove u/v degeneracy and sort by increasing eigenvalue
+% note that sin/cos degeneracy remains
+pev = ev(:, ew>0);  [~, i] = sort(pew);
+ew = pew(i);  ev = pev(:,i);
+
+evIm = sqrt(sum(imag(ev).^2));
+figure, plot(evIm, '.k')
+title('Imaginary components of BdG eigenvectors')
+ylabel('|Im u_i|^2')
+
+% Fix Matlab quirk where four modes are elliptically polarised
+bmod = U*ev;
+ixodd = find(evIm > 0.1);
+for i = 1:2:length(ixodd)
+	ix = ixodd(i);
+	bmod(:,ix:ix+1) = [real(bmod(:,ix)) imag(bmod(:,ix))];
+	bmod(:,ix) = bmod(:,ix) / norm(bmod(:,ix));
+	bmod(:,ix+1) = bmod(:,ix+1) / norm(bmod(:,ix+1));
+end
 buv = reshape(bmod,gs,2,[]);
 
-figure, plot(r.x, LAP*a, r.x, K*a, r.x, r.a.g*diag(a.^2)*a)
+fprintf('non-orthonormality of corrected eigenvectors: %.2e\n', ...
+	norm(bmod'*bmod-eye(gs-1), 'fro'))
 
-figure, semilogy(abs(ew), '.k'), title Eigenvalues
-% w = ck, k is around n/4L, healing length is (2*gamma)^{-1/2} in LL normalisation
-% CHECKME
+figure
+nodd = nnz(ixodd);  odd = buv(:, :, ixodd);  wodd = ew(ixodd);
+for i = 1:nodd, for j = 1:2
+	subplot(nodd, 2, 2*i+j-2)
+	plot(r.x, real(odd(:,j,i)), '-k', r.x, imag(odd(:,j,i)), '-r')
+	title(num2str(wodd(i)))
+end, end
+
+% Eigenstuff is real below here
+bmod = real(bmod);  buv = real(buv);  ev = real(ev);
+
+% figure, plot(r.x, LAP*a, r.x, K*a, r.x, r.a.g*diag(a.^2)*a)
+
+figure, subplot 211, plot(ew, '.k'), title('computed and expected BdG eigenvalues')
+% w = ck, k is around n/2L, healing length is (2*gamma)^{-1/2} in LL normalisation
+% FIXME include ordinary energy
 n = 1:length(ew);  L = r.ranges(2);  L = L*gs/(gs-1);
-hold on, plot(n, (n-2)*sqrt(r.a.gamma)/(4*L), '-k')
-legend computed sound Location SouthEast
+k1 = 2*pi/L;  k = k1*n/2;  heal = 1/sqrt(2*r.a.gamma);
+subplot 212, plot(n, k.*sqrt(heal.^-2+k.^2), '-k')
+% plot(n, (n-2)*sqrt(r.a.gamma)/(4*L), '-k')
 
-figure, imagesc(bmod), colormap gray, title Eigenvectors
+figure, subplot 211, imagesc(squeeze(buv(:,1,:))), title 'BdG u modes'
+subplot 212, imagesc(squeeze(buv(:,2,:))), title 'BdG v modes'
+xlabel mode, ylabel x
+colormap gray
 
 rsdl = sqrt(sum(abs(BdG*bmod-bmod*diag(ew)).^2));
 ix = 1:numel(ew);  iy = ix(ew>0);  iz = ix(ew<0);
@@ -31,7 +80,7 @@ figure, semilogy(nms, '.k'), title 'Eigenvector norms'
 figure, plot(r.x, r.a.g*a.^2-mu, '-k', r.x, r.a.K(r), '-r')
 legend('\mu discrep.', 'K')
 
-k1 = 2*pi/L;
+
 
 [sv,sw] = eig(-LAP, 'vector');
 n = 1:length(sw);  k = k1*n/2;
@@ -44,7 +93,7 @@ figure, plot(real(osw), '.k'), title('orthogonal laplacian eigenvalues')
 figure, imagesc(real(U1*osv)), colormap gray, title('orthogonal laplacian eigenvectors')
 
 figure
-k1 = 2*pi/L;  k2 = 10*k1;
+k2 = 10*k1;
 plot(r.x, sin(k1*r.x'), '-k', r.x, -k1^-2*LAP*sin(k1*r.x'), '--r')
 hold on, 
 plot(r.x, sin(k2*r.x'), '-k', r.x, -k2^-2*LAP*sin(k2*r.x'), '--r')
