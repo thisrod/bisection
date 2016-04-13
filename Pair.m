6e63e0d834f4251dc721fc97359d883ef80e2965
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-04-13 15:55:08 +1000

    changed Pair to test TW noise

--- Pair.m
+++ Pair.m
@@ -2,21 +2,23 @@
 
 clear system gsop
 
-system.name = 'free gas in one dimension';
+system.name = 'free gas in one dimension, 70 modes';
 system.a.gamma = 1e3;
 system.a.N = 1e5;
+system.points = [nan 70];
+system.ensembles = 1e3;
 system = trap(system);
 
 gsop = order(system, 1e5);
 gsop.ranges(1) = 0.5;
-gsop.points = [49 70];
+gsop.points(1) = 49;
 gsop.steps = 30;
-gsop = xinstrument(gsop, 'n', 'N', 'T', 'K', 'V', @(~,in) in.a.g, 'g2');
+% gsop = xinstrument(gsop, 'n', 'N', @(~,in) in.a.N, 'T', 'K', 'V', @(~,in) in.a.g, 'g2');
+gsop = xinstrument(gsop, 'N');
 
-ground = system;
-ground.ranges(1) = 0.5;
-ground.points = [49 70];
-ground = bdg(ground, 0);
+coherent = twop(static(system, 0));
+coherent = xinstrument(coherent, 'N', @(~,in) in.a.N + in.points(2)/2, 'Re', 'Im');
+ground = bdg(system, 0);
 
 % xspde(gsop)
-xspde({gsop, ground})
+xspde({gsop, coherent})
--- twop.m
+++ twop.m
@@ -3,16 +3,18 @@ function out = twop(in)
 % out.transfer adds Wigner noise to the input order parameter
 % this assumes that the order parameter is normalised to the particle number
 
-out.name = 'Bogoliubov initial state';
+out.name = 'coherent initial state';
 for s = {'dimension', 'fields', 'ranges', 'c', 'a', 'points'}
 	if isfield(in, s{1}), out.(s{1}) = in.(s{1}); end
 end
 
 out.points(1) = 2;  out.ranges(1) = 1;
-out.transfer = @(w,r,a0,r0) tfr(t,w,r,a0,r0);
+out.noises = 2;
+out.transfer = @tfr;
 
 end
 
-function a = tfr(t,w,r,a0,r0)
-	a = a0 + w;
+function a = tfr(w,r,a0,r0)
+	
+	a = a0 + [1 1i]*w/2;
 end
