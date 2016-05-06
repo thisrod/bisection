1400bd4c4ce866f2e42f3f95997ac15e84fd468f
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-05-06 15:16:14 +1000

    refactored bdg to buv

--- Pair.m
+++ Pair.m
@@ -1,4 +1,4 @@
-% one dimensional BEC pair correlations
+% shell to run order and buv for a one-dimensional free gas.
 
 clear system gsop coherent 
 
@@ -13,12 +13,16 @@ gsop = order(system, 1e5);
 gsop.ranges(1) = 0.5;
 gsop.points(1) = 49;
 gsop.steps = 30;
+gsop.raw = true;
 % gsop = xinstrument(gsop, 'n', 'N', @(~,in) in.a.N, 'T', 'K', 'V', @(~,in) in.a.g, 'g2');
-gsop = xinstrument(gsop, 'N');
+gsop = xinstrument(gsop, 'n', 'N', 'Kx');
 
 coherent = twop(static(system, 0));
 coherent = xinstrument(coherent, 'ntw', 'g2tw');
-ground = bdg(system);
+% ground = bdg(system);
 
 % xspde(gsop)
-[~, ~, rslt] = xspde({gsop, ground});
+[~, out, rslt, raw] = xsim(gsop);
+rslt = rslt{1};
+a = squeeze(raw{1,1,2}(:,:,end));  a = a(:);
+kix = find(strcmp(gsop.olabels, 'K^2'));  K = squeeze(rslt{kix}(1,end,:));
