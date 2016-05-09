39156ba06c6922e9e1738876dc094b11f0b4eaf9
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-05-09 11:22:18 +1000

    updated docstring for ground

--- ground.m
+++ ground.m
@@ -2,11 +2,8 @@ function out = ground(in)
 %BDG     XPDE structure to draw Wigner samples of the ground state
 %
 %   % GROUND(in) takes an XPDE input structure in of the form returned by
-%   STATIC. It returns a similar input structure. This structure runs
-%   after a BDG structure. It generates samples of the field according to
+%   STATIC. It returns a similar input structure. This structure is annotated with sound wave modes, and its inital function generates samples of the field according to
 %   in.ensembles and in.a.T.
-%
-%   See also BDG.
 
 % in.a.T is the condensate temperature, expressed as a square wavenumber in dispersion units.
 
