da7fa5c8baf3641918768ab0c42d59e2202b2d8c
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-05-18 17:12:10 +1000

    finish moving files

--- static.m
+++
@@ -1,9 +0,0 @@
-function in = static(in, t)
-%STATIC	Transform a dynamic atomic trap structure to be stuck at a fixed time
-%
-%    static(in, t)
-
-in.name = sprintf('%s at time %f', in.name, t);
-K = in.a.K;  in.a.K = @(r) K(setfield(r, 't', t));
-
-end
