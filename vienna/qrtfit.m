7509f9afab0304eee7adc0c0f856eedeb55a2cb8
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-05-26 17:12:57 +1000

    fixed all plots

--- vienna/qrtfit.m
+++ vienna/qrtfit.m
@@ -6,15 +6,21 @@ prinax
 % (i-1)ms, in the order [x^4 1 x^2 y^2 z^2]
 cfs = nan(5,18);
 
+far = 70;	% z index of far from origin section
+
 tplot = [0 15 17];
 
 [~, x, y, z] = loadk(0);
+samples.x = nan(18, length(x));
+samples.y = nan(18, length(y));
+samples.z = nan(18, length(z));
+samples.K = nan(18, length(x), length(y), length(z));
 Ks = zeros([length(tplot) length(x) length(y) 2]);
 Ks1 = zeros([length(tplot) length(x)]);
 zsec = zeros([length(tplot) 2]);
 
 for t = 0:17
-	K = loadk(t);
+	K = loadk(t);  Kt(t+1,:,:,:) = K;
 	[r0, wgt] = tcent(x,y,z,K);  
 	
 	% shift origin to trap centre
@@ -41,72 +47,84 @@ for t = 0:17
 	rsdl = K(wix) - [w(:,1).^4 ones(size(w(:,1))) w(:,1).^2]*cfs(1:3, t+1);
 	cfs(4:5, t+1) = ([w(:,2).^2 w(:,3).^2].*[wgt wgt]) \ (rsdl.*wgt);
 	
+	% Save samples for comparision plots
+	samples.x(t+1,:) = x;  samples.y(t+1,:) = y;  samples.z(t+1,:) = z;
+	samples.K(t+1,:,:,:) = K;
+	
 	[~,i] = ismember(t, tplot);
 	if i
-		Ks(i,:,:,:) = K(:,:,[k0 70]);  Ks1(i,:) = Kx;
-		% Kfit = [r(:,1).^4 ones(size(r(:,1))) r.^2]*cfs(:,t+1);
-		% Kfit = reshape(Kfit, size(K));
-		% Kf(i,:,:,:) = Kfit(:,:,[k0 70]);
-		zsec(i,:) = z([k0 70]);
+		Ks(i,:,:,:) = K(:,:,[k0 far]);  Ks1(i,:) = Kx;
+		zsec(i,:) = z([k0 far]);
 	end
 end
 
-xoff = [0 0 z(70)]*U;  x = [x; x+xoff(1)];
-
-% set x^4 coefficient constant - juggle this to get plots that fit
+% make x^4 coefficient constant.  The averaged range is adjusted
+% so that the double well plots fit
 cfs(1,:) = mean(cfs(1,15:end));
 
-% recompute fit with constant coeff of x^4
-L = 1;  N = 16;  h = 2*L/N;  
-x2 = h*(1:3*N)-3*L;  y2 = h*(1:N)-L; 
-Kf = zeros([length(tplot) length(x2) length(y2) 2]);
-for i = 1:length(tplot)
-	t = tplot(i);
-	[X,Y,Z] = ndgrid(x2,y2,zsec(i,:));
-	r = [X(:) Y(:) Z(:)]*U;
-	Kfit = [r(:,1).^4 ones(size(r(:,1))) r.^2]*cfs(:,t+1);
-	Kf(i,:,:,:) = reshape(Kfit, size(X));
-end
+Ks = samples.K(tplot+1, :, :, [k0 far]);
+Ks1 = squeeze(samples.K(tplot+1, :, j0, k0));
+
+xoff = [0 0 z(far)]*U;  x = [x; x+xoff(1)];
+
 
 save cfs.mat cfs
 
-% save qrtfitdat.mat x y x2 y2 xoff zsec tplot cfs Ks Ks1 Kf
-
 set(0, 'defaultaxesfontsize', 14, 'defaulttextfontsize', 14)
 
+% axes for line and section plots
 xx = linspace(-2.2, 2.2, 100)';
+L = 1;  N = 16;  h = 2*L/N;
+x2 = h*(1:3*N)-3*L;  y2 = h*(1:N)-L; 
+
+% quadratic levels to space harmonic trap contours evenly
 ctrs = (1:10).^2/0.25.^2;
 
-for i = 1:length(tplot)
+for t = [0 15 17]
 
-t = tplot(i);
+% relocate trap centre
+[~,i0] = min(abs(samples.x(t+1,:)));
+[~,j0] = min(abs(samples.y(t+1,:)));
+[~,k0] = min(abs(samples.z(t+1,:)));
 
-figure, plot(x(1,:), Ks1(i,:), '.k')
+figure, plot(samples.x(t+1,:), squeeze(samples.K(t+1, :, j0, k0)), '.k')
+title(['Fit along splitting axis at t = ' int2str(t) ' ms'])
 axis([-2.2 2.2 0 200]), hold on
 plot(xx, [xx.^4 ones(size(xx)) xx.^2]*cfs(1:3, t+1), '-k')
-title(['Fit along splitting axis at t = ' int2str(t) ' ms'])
 s = sprintf('K^2 = %.2f x^4 %+.2f x^2 %+.2f', cfs(1, t+1), cfs(3, t+1), cfs(2, t+1));
 text(-1.5, 125, s)
 xlabel x, ylabel K^2
 
 % TODO get Matlab to put a box around the figure
 
-for j = 1:2
+% plot contours of samples and fit at trap centre and end of z axis
+for k = [k0 far]
+	% find shifted x axis
+	x = samples.x(t+1,:)';
+	r = [x zeros(size(x)) repmat(samples.z(t+1,k), size(x))]*U;
+	x = r(:,1)';
+	
+	% compute fitted potential
+	[X,Y,Z] = ndgrid(samples.x(t+1,:), samples.y(t+1,:), samples.z(t+1,k));
+	r = [X(:) Y(:) Z(:)];
+	Kfit = [X(:).^4 ones(size(X(:))) r.^2]*cfs(:,t+1);
+	Kfit = squeeze(reshape(Kfit, size(X)));
+	
 	figure
-	contour(x(j,:), y, squeeze(Ks(i,:,:,j))', ctrs, '-k')
+	contour(x, samples.y(t+1,:), squeeze(samples.K(t+1,:,:,k))', ctrs, '-k')
 	hold on
-	contour(x2, y2, squeeze(Kf(i,:,:,j))', ctrs, ':k')
+	contour(samples.x(t+1,:), samples.y(t+1,:), Kfit', ctrs, ':k')
 	% xlabel x, ylabel y
 	axis equal, axis([-4 4 -3 3]), axis off
 	% title(sprintf('Fit at t = %d, z = %.1f', t, zsec(i,j)))
-	if i == 1
+	if t == 0
 		patch([-3.8 -3.8 -2.8 -2.8], [-1 -1.15 -1.16 -1], 0.5*[1 1 1])
 		text(-3.5,-0.8,'1 \mum')
 	end
-	if j == 1, saveTightFigure(['kmode' int2str(t) '.pdf']), end
+	if k == k0, saveTightFigure(['kmode' int2str(t) '.pdf']), end
 end
 
-end	% i = 1:length(tplot)
+end	% for t
 
 figure
 plot(0:17, cfs(1:4,:)), hold on
