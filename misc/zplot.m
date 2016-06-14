efa9687bd481d67071dfbde1ebbc8482e3e0e880
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-06-14 15:13:50 +1000

    changed zplot to take meshgrid arrays

--- misc/zplot.m
+++ misc/zplot.m
@@ -1,7 +1,8 @@
 function h = zplot(x, y, w, cmap)
 %zplot Plot a function of a complex variable as a color image
 %   zplot(x, y, w)
-%   x and y are as for image.  w is a matrix of samples f(x+iy).
+%   x and y are as for image.  w is a matrix of samples f(xx+i*yy),
+%   where xx and yy are in meshgrid order.
 %   The modulus of f is plotted on the Argand plane as brightness, 
 %   its argument as hue.  Colors might be excessively quantised.
 %
@@ -13,7 +14,7 @@ if nargin < 4, cmap = phase(128); end
 ff = linspace(0, 2*pi, size(cmap,1));
 w = w(:);  f = mod(angle(w), 2*pi);
 pixels = repmat(abs(w)/max(abs(w)), 1, 3).*interp1(ff, cmap, f);
-pixels = reshape(pixels, numel(x), numel(y), 3);
+pixels = reshape(pixels, numel(y), numel(x), 3);
 image(x, y, pixels)
 h = gca;
 set(h, 'YDir', 'normal')
