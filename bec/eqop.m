dff5c48a2b8c1bea1c05d369efaf6db541b03853
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2016-06-20 16:49:01 +1000

    eqop annotates energies

--- bec/eqop.m
+++ bec/eqop.m
@@ -1,7 +1,7 @@
 function out = eqop(in, debug)
 %EQOP     annotate input structure with equilibrium order parameter
 %
-%    in = EQOP(in) runs xsim to compute an equilibrium order parameter, and returns a structure annotated with op, T, K, R, and healing.
+%    in = EQOP(in) runs xsim to compute an equilibrium order parameter, and returns a structure annotated with op, T, K, and R.
 %
 %    in = EQOP(in, 'debug') plots stuff that helps for debugging
 %
@@ -9,15 +9,14 @@ function out = eqop(in, debug)
 
 debug = (nargin == 2 && strcmp(debug, 'debug'));
 
-% data = {'T', 'K', 'R', 'healing'};
-data = {'Re', 'Im'};	% stubs before observables are implemented
+data = {'T', 'K', 'R'};
 
 out = in;
 in = order(in);
 in.raw = true;
 
 obs = data;
-if debug, obs = [obs 'N' 'n' 5]; end
+if debug, obs = [obs 'N' 'n' 'nk']; end
 in = xinstrument(in, obs{:});
 
 [~, in, rslt, raw] = xsim(in);
@@ -29,7 +28,7 @@ a = squeeze(raw{1,2}(1,1,end,:));  a = a(:);  out.a.op = a;
 for o = data
 	o = o{:};
 	r = rslt{find(strcmp(in.olabels, o))};
-	out.a = setfield(out.a, o, squeeze(r(1,1,end,:)));
+	out.a = setfield(out.a, o, squeeze(r(1,1,end,1)));
 end
 
 % out.dV = in.dV;		% bogs used to need this
--- bec/eqopTest.m
+++ bec/eqopTest.m
@@ -1,6 +1,4 @@
-% not yet set up as test cases
-
-clear notrap hmtrap
+clear notrap hmtrap box
 
 notrap.name = 'no trap';
 notrap.a.gamma = 1;
@@ -10,9 +8,9 @@ notrap.ranges = [10 nan];
 notrap.steps = 30;
 notrap = trap(notrap);
 
-% find out why n goes negative
+% watch out: the large potential, combined with XSPDE's invisible extrapolation, can give bizarre results when the fine grid has converged but the coarse one is unstable.
 
-box.name = 'box';
+box.name = 'unit length box';
 box.a.g = 0;
 box.a.K = @(r) 1e3*(abs(r.x)>1/2);
 box.a.N = 1;
@@ -21,12 +19,12 @@ box.ranges = [10 2];
 box.steps = 60;
 box = trap(box);
 
-hmtrap.name = 'harmonic';
+hmtrap.name = 'harmonic with l_0 = 1';
 hmtrap.a.g = 0;
 hmtrap.a.K = @(r) r.x.^2;
 hmtrap.a.N = 1;
 hmtrap.points = [49 70];
-hmtrap.ranges = [10 5];
+hmtrap.ranges = [10 20];
 hmtrap.steps = 30;
 hmtrap = trap(hmtrap);
 
@@ -36,4 +34,18 @@ hmtrap = eqop(hmtrap, 'debug');
 
 box = eqop(box, 'debug');
 
-figure, plot(1:70, real(box.a.op), '-k', 1:70, imag(box.a.op), '-r')
+%% potential energy is small in a box
+
+assert(abs(box.a.K - 0) < 10);
+
+%% kinetic energy for the box ground state
+
+assert(abs(box.a.T - pi^2) < 1)
+
+%% potential energy for harmonic
+
+assert(abs(hmtrap.a.K - 0.5) < 1e-3);
+
+%% kinetic energy for harmonic
+
+assert(abs(hmtrap.a.T - 0.5) < 1e-3);
